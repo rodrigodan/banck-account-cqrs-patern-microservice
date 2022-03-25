b03461043e8a69f53ba9e0dd5cecdf4877cf3751
rgdaniel <rgdaniel@indracompany.com>
2022-03-25 10:01:18 -0300

    CQRS Pattern Basic architecture setup, Command Dispatching from cqrs implemented using the  Mediator Pattern

---
+++ back-account/account.cmd/src/main/java/com/techbank/account/cmd/api/DepositFunds.java
@@ -0,0 +1,10 @@
+package com.techbank.account.cmd.api;
+
+import com.techbank.cqrs.core.commands.BaseCommand;
+import lombok.Data;
+
+@Data
+public class DepositFunds extends BaseCommand {
+    private double amount;
+
+}
---
+++ back-account/account.cmd/src/main/java/com/techbank/account/cmd/api/OpenAccountCommand.java
@@ -0,0 +1,14 @@
+package com.techbank.account.cmd.api;
+
+import com.techbank.account.common.dto.AccountType;
+import com.techbank.cqrs.core.commands.BaseCommand;
+import lombok.Data;
+
+@Data
+public class OpenAccountCommand extends BaseCommand {
+
+    private String accountHolder;
+    private AccountType accountType;
+    private double openingBalance
+
+}
